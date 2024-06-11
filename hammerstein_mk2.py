db116a6bc83055e3f29fb4f1984dbe9ad5b3d07b
JonnyAlpha <billharvs@btinternet.com>
2024-06-11 19:22:25 +0100

    Add files via upload
    Added corrected version of hammerstein_mk2.py

--- hammerstein_mk2.py
+++ hammerstein_mk2.py
@@ -1,17 +1,58 @@
 # Hammerstein Mk2 - a mobile robot
+# Before running install somne dependencies:
+# pip install pygame
+# pip install python-vlc
+# pip install gpiozero - if using any gpio pins
+# pip install vosk
+# pip3 install pyaudio
+# pip3 install pyttsx3
 
 # Import the required libraries
 import pygame # for playing audio files
 import vlc # for playing videos
-import cv2
 from time import sleep
 import os # to run terminal commands
-import sys
-import keyboard # only for testing 
-from threading import Thread
+from gpiozero import MotionSensor
+from vosk import Model, KaldiRecognizer
+import pyaudio
+import pyttsx3
+
+# Setup the motion sensor
+pir = MotionSensor(23)
+
+# Initialise VLC player
+instance = vlc.Instance()
+player = instance.media_player_new()
+
+# Setup voice recognition
+model = Model(r"/home/pi/HammersteinMk2/vosk-model-small-en-us-0.15")
+recognizer = KaldiRecognizer(model, 16000)
+
+mic = pyaudio.PyAudio()
+
+engine = pyttsx3.init()
+
+listening = False
+
+def get_command():
+    listening = True
+    stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+    while listening:
+        stream.start_stream()
+        try:
+            data = stream.read(4096)
+            if recognizer.AcceptWaveform(data):
+                result = recognizer.Result()
+                response = result[14:-3]
+                listening = False
+                stream.close()
+                return response
+        except OSError:
+            pass
+
 
 def startup():
-    print("Hammerstein MkII \n")
+    print("HAMMERSTEIN MKII \n")
     sleep(0.5)
     print("Starting up. \n")
     sleep(0.5)
@@ -28,13 +69,20 @@ def startup():
     print("System ready")
 
     pygame.mixer.init()
-    sound = pygame.mixer.Sound('/Users/BillHarvey/Documents/Electronics_and_Robotics/Hammerstein_Mk2/audio/mac_startup.wav')
+    sound = pygame.mixer.Sound('/home/pi/HammersteinMk2/audio/mac_startup.wav')
     playing = sound.play()
     while playing.get_busy():
         pygame.time.delay(100)
     
+def welcome():
+    #Create media objects 
+    media = instance.media_new('/home/pi/HammersteinMk2/videos/welcome.mp4')
     
+    # Play video
+    #player.toggle_fullscreen()
+    play_video(player, media)
 
+    
 def play_video(player, media):
     # Play video
 
@@ -42,66 +90,62 @@ def play_video(player, media):
     player.play()
                 
     while player.get_state() != vlc.State.Ended:
-        sleep(1)
+        sleep(5)
 
-def check_motion():
+def wait_motion():
+    media = instance.media_new('/home/pi/HammersteinMk2/videos/whos_there.mp4')
 
-    if keyboard.read_key() == "m": # waits for a keyboard press, if its 'm' motion is detected   
-        motion_detected = True
-        print("motion detected!")
+    while True:
+        # Check for motion
+        #if pir.motion_detected:
+         #   play_video(player, media)
+        #pir.when_motion = print("motion"), play_video(player, media)
+        #pir.when_no_motion = print("still"), sleep(1)
+        pir.wait_for_motion()
+        play_video(player, media)
+        break
 
-    else: # any key other than 'm' has been pressed
-
-        motion_detected = False
-
-    return motion_detected
-
-def holly_still():
-    # display the lobby image
-    img = cv2.imread("Holly.jpeg", cv2.IMREAD_ANYCOLOR)
-    
-    cv2.imshow("Holly", img)
-    sleep(2)
-    cv2.waitKey(0) #wait for ESC key
-    #sys.exit()
-
-def welcome():
-    # Initialise VLC player
-    instance = vlc.Instance()
-    player = instance.media_player_new()
-
-    #Create media objects 
-    media = instance.media_new('/Users/BillHarvey/Documents/Electronics_and_Robotics/Hammerstein_Mk2/videos/welcome.mp4')
-    
-    # Play video
+def hello():
+    media = instance.media_new('/home/pi/HammersteinMk2/videos/hello_bill.mp4')
     play_video(player, media)
-    print("Entering wait mode")
-    media1 = instance.media_new('/Users/BillHarvey/Documents/Electronics_and_Robotics/Hammerstein_Mk2/videos/whos_there.mp4')
-    media2 = instance.media_new('/Users/BillHarvey/Documents/Electronics_and_Robotics/Hammerstein_Mk2/videos/hello_bill.mp4')
-    while True:  
-        motion_detected = check_motion()  
-        if motion_detected == True:
-            play_video(player, media1)
-        else:
-            #Thread(target = face_detect).start()
-            #Thread(target = vid_play).start()
-            #os.system("python3 demo_minimal.py")
-            #play_video(player, media2)
-            #sleep(1)
-            #break
-            #play_video(player, media)
+    media = instance.media_new('/home/pi/HammersteinMk2/videos/what_would_you_like.mp4')
+    play_video(player, media)
 
-def face_detect():
-    os.system("python3 demo_minimal.py")
-def vid_play():
-    play_video(player, media2)
-    
+def normal_operation():
+    hello()
+    while True:
+        print("Waiting for voice command...")
+        command = get_command()
+        if command == "":
+            pass
+        elif command == "hello":
+            #engine.say("Hello to you too")
+            #engine.runAndWait()
+            media = instance.media_new('/home/pi/HammersteinMk2/videos/hello_bill.mp4')
+            play_video(player, media)
+        elif command == "say something":
+            engine.say("something")
+            engine.runAndWait()
+        elif command == "what is your name":
+            engine.say("Holly")
+            engine.runAndWait()
+        elif command == "end":
+            media = instance.media_new('/home/pi/HasmmersteinMk2/videos/I_can_do_that.mp4')
+            sleep(4)
+            break
+        else:
+            #engine.say("Sorry I don't understand that yet")
+            #engine.runAndWait()
+            media = instance.media_new('/home/pi/HammersteinMk2/videos/I_cant_do_that.mp4')
+            play_video(player, media)
+            
 def main():
     startup()
-    holly_still()
     welcome()
+    wait_motion()
+    normal_operation()
+    
+    
+    
 
 main()
-
-cv2.destroyAllWindows() 
-
